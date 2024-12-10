089164c10d52344e588d5d4f36fd2a3b536b0267
Joanna Gorniak <joanna.gorniak.00@gmail.com>
2024-12-10 22:35:41 +0100

    bind events added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   const tasks = [];
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({ content: newTaskContent });
+    tasks.push({ content: newTaskContent, done: false });
     render();
   };
 
@@ -11,34 +11,49 @@
     render();
   };
 
-  
-  
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  };
+
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
-              <li class="section__listItem">
-                <button class="section__button js-done">
-                  ${task.done ? "&#10004" : "&#x1F5D1"}
-                </button>
-                <span class="${task.done ? "section__listItem--done" : ""}">
-                  ${task.content}
-                </span>
-                <button class="section__button section__buttonRemove js-remove">
-                  &#x1F5D1
-                </button>
-              </li>
-            `;
+          <li class="section__listItem">
+            <button class="section__button js-done">
+              ${task.done ? "&#10004" : ""}
+            </button>
+            <span class="${task.done ? "section__listItem--done" : ""}">
+              ${task.content}
+            </span>
+            <button class="section__button section__buttonRemove js-remove">
+              &#x1F5D1
+            </button>
+          </li>
+        `;
     }
+
     document.querySelector(".js-taskList").innerHTML = htmlString;
 
-    const removeButtons = document.querySelectorAll(".js-remove");
-  
-  removeButtons.forEach(removeButtons, index) => {
-    removeButton.addEventLitsener("click", () => {
-      removeTask(index);
-    }); 
+    bindEvents(); // Ponowne przypisanie eventów
   };
 
   const onFormSubmit = (event) => {
